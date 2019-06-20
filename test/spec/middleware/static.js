51717e08638c1cb8baf06e29a077a2080ee9735a
Anton <anton@adc.sh>
2019-06-20 19:13:40 +0300

    all static tests

--- test/spec/middleware/static.js
+++ test/spec/middleware/static.js
@@ -30,67 +30,64 @@ const T = {
       .get('/test/chapter2.txt')
       .assert(200, body)
   },
-  // async 'serves static files from multiple roots'({
-  //   startApp, staticDir, staticDir2, readStaticFixture, readStaticFixture2,
-  // }) {
-  //   const body = await readStaticFixture()
-  //   const body2 = await readStaticFixture2()
-  //   const { url } = await startApp({
-  //     static: {
-  //       use: true,
-  //       root: [staticDir, staticDir2],
-  //     },
-  //   })
-  //   const fullUrl = `${url}/chapter2.txt`
-  //   const res = await rqt(fullUrl)
-  //   equal(res, body)
-  //   const fullUrl2 = `${url}/chapter3.txt`
-  //   const res2 = await rqt(fullUrl2)
-  //   equal(res2, body2)
-  // },
-  // async 'serves static files from multiple roots from mount'({
-  //   startApp, staticDir, staticDir2, readStaticFixture, readStaticFixture2,
-  // }) {
-  //   const body = await readStaticFixture()
-  //   const body2 = await readStaticFixture2()
-  //   const { url } = await startApp({
-  //     static: {
-  //       use: true,
-  //       root: [staticDir, staticDir2],
-  //       mount: '/test',
-  //     },
-  //   })
-  //   const fullUrl = `${url}/test/chapter2.txt`
-  //   const res = await rqt(fullUrl)
-  //   equal(res, body)
-  //   const fullUrl2 = `${url}/test/chapter3.txt`
-  //   const res2 = await rqt(fullUrl2)
-  //   equal(res2, body2)
-  // },
-  // async 'can specify multiple configurations'({
-  //   startApp, staticDir, staticDir2, readStaticFixture, readStaticFixture2,
-  // }) {
-  //   const body = await readStaticFixture()
-  //   const body2 = await readStaticFixture2()
-  //   const { url } = await startApp({
-  //     static: [
-  //       {
-  //         use: true,
-  //         root: staticDir,
-  //       },
-  //       {
-  //         use: true,
-  //         root: staticDir2,
-  //       },
-  //     ],
-  //   })
-  //   const fullUrl = `${url}/chapter2.txt`
-  //   const res = await rqt(fullUrl)
-  //   equal(res, body)
-  //   const fullUrl2 = `${url}/chapter3.txt`
-  //   const res2 = await rqt(fullUrl2)
-  //   equal(res2, body2)
-  // },
+  async 'serves static files from multiple roots'({
+    startApp, createApp, staticDir, staticDir2, readStaticFixture, readStaticFixture2,
+  }) {
+    const body = await readStaticFixture()
+    const body2 = await readStaticFixture2()
+    await createApp({
+      static: {
+        use: true,
+        root: [staticDir, staticDir2],
+      },
+    })
+    await startApp()
+      .get('/chapter2.txt')
+      .assert(200, body)
+      .get('/chapter3.txt')
+      .assert(200, body2)
+  },
+  async 'serves static files from multiple roots from mount'({
+    startApp, createApp, staticDir, staticDir2, readStaticFixture, readStaticFixture2,
+  }) {
+    const body = await readStaticFixture()
+    const body2 = await readStaticFixture2()
+    await createApp({
+      static: {
+        use: true,
+        root: [staticDir, staticDir2],
+        mount: '/test',
+      },
+    })
+    await startApp()
+      .get('/test/chapter2.txt')
+      .assert(200, body)
+      .get('/test/chapter3.txt')
+      .assert(200, body2)
+  },
+  async 'can specify multiple configurations'({
+    startApp, createApp, staticDir, staticDir2, readStaticFixture, readStaticFixture2,
+  }) {
+    const body = await readStaticFixture()
+    const body2 = await readStaticFixture2()
+    await createApp({
+      static: [
+        {
+          use: true,
+          root: staticDir,
+        },
+        {
+          use: true,
+          root: staticDir2,
+        },
+      ],
+    })
+    await startApp()
+      .get('/chapter2.txt')
+      .assert(200, body)
+      .get('/chapter3.txt')
+      .assert(200, body2)
+  },
 }
 
 export default T
