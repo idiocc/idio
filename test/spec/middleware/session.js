279b96ed787da50c2b9f5f98b8023be67772f788
Anton <anton@adc.sh>
2019-07-18 16:32:22 +0300

    test keygrip

--- test/spec/middleware/session.js
+++ test/spec/middleware/session.js
@@ -1,4 +1,6 @@
 import Context from '../../context'
+import { throws } from '@zoroaster/assert'
+import { Keygrip } from '../../../src'
 
 /** @type {Object.<string, (c: Context)>} */
 const T = {
@@ -27,6 +29,43 @@ const T = {
       .get('/').assert(200, 'hello')
       .get('/exit').assert(204)
       .get('/').assert(200, 'no cookie')
+  },
+  async 'accepts keygrip instance'({ createApp, startApp }) {
+    await createApp({
+      session: {
+        use: true,
+        keys: new Keygrip(['a']),
+      },
+      test(ctx) {
+        if (ctx.path == '/set') {
+          ctx.session.test = 'hello'
+          ctx.status = 204
+        } else if (ctx.path == '/exit') {
+          ctx.session = null
+          ctx.status = 204
+        } else {
+          ctx.body = ctx.session.test || 'no cookie'
+        }
+      },
+    })
+    await startApp()
+      .session()
+      .get('/set').assert(204)
+      .get('/').assert(200, 'hello')
+      .get('/exit').assert(204)
+      .get('/').assert(200, 'no cookie')
+  },
+  async 'throws when keys are not passed'({ createApp }) {
+    await throws({
+      fn: createApp,
+      args: [{
+        session: {
+          use: true,
+          keys: 123,
+        },
+      }],
+      message: /Keys must be an array or an instance of Keygrip \/ child classes/,
+    })
   },
 }
 
