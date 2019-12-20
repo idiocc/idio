56bcd550079cd3401934365bfcb43e810d654242
Anton <anton@adc.sh>
2019-12-20 08:00:07 +0300

    upd goa types

--- types/goa/typedefs/application.js
+++ types/goa/typedefs/application.js
@@ -4,6 +4,7 @@ export {}
 /**
  * @typedef {import('http').IncomingMessage} http.IncomingMessage
  * @typedef {import('http').ServerResponse} http.ServerResponse
+ * @typedef {import('http').Server} http.Server
  * @typedef {import('events').EventEmitter} events.EventEmitter
  * @typedef {import('../../../compile').Keygrip} _goa.Keygrip
  * @typedef {import('../../../compile').Context} _goa.Context
@@ -12,20 +13,24 @@ export {}
  * @typedef {_goa.Application} Application `＠interface` The application interface.
  * @typedef {events.EventEmitter & _goa.$Application} _goa.Application `＠interface` The application interface.
  * @typedef {Object} _goa.$Application `＠interface` The application interface.
- * @prop {boolean} [proxy] Whether the server is running behind a proxy.
- * @prop {!Array<!_goa.Middleware>} middleware The array with middleware used on the server.
+ * @prop {boolean} [proxy=false] Whether the server is running behind a proxy. Default `false`.
+ * @prop {!Array<!_goa.Middleware>} middleware The array with middleware used on the server. Default `[]`.
  * @prop {!_goa.Context} context The context object for each request.
  * @prop {!_goa.Request} request The request object for each request.
  * @prop {!_goa.Response} response The response object for each request.
  * @prop {string} [env="development"] The value from the `NODE_ENV` environment variable. Default `development`.
  * @prop {!(_goa.Keygrip|Array<string>)} [keys] The keys for signing of the cookies.
  * @prop {boolean} [silent=false] Whether to not log an error when it happens. Default `false`.
- * @prop {() => function(!http.IncomingMessage, !http.ServerResponse)} callback Returns the request handler callback for node's native http/http2 server composed of the installed middleware.
- * @prop {(arg0: !http.IncomingMessage, arg1: !http.ServerResponse) => ?} createContext `＠private` Initialize a new context.
- * @prop {number} [subdomainOffset] For example, if the domain is "tobi.ferrets.example.com": If `app.subdomainOffset` is not set, request.subdomains is `["ferrets", "tobi"]`. If `app.subdomainOffset` is 3, request.subdomains is `["tobi"]`.
- * @prop {() => ?} listen Shorthand for: `http.createServer(app.callback()).listen(...)`
+ * @prop {number} [subdomainOffset=2] For example, if the domain is _tobi.ferrets.example.com_:
+ * - if `app.subdomainOffset` is not set, request.subdomains is `["ferrets", "tobi"]`.
+ * - if `app.subdomainOffset` is 3, request.subdomains is `["tobi"]`. Default `2`.
+ * @prop {(...args: *[]) => !http.Server} listen Shorthand for: `http.createServer(app.callback()).listen(...)`.
  * @prop {(middleware: !_goa.Middleware) => !_goa.Application} use Use the given middleware `fn`. Old-style middleware will be converted.
+ * @prop {() => function(!http.IncomingMessage, !http.ServerResponse)} callback Returns the request handler callback for Node's native _http/http2_ server composed of the installed middleware.
+ * @prop {(req: !http.IncomingMessage, res: !http.ServerResponse) => !_goa.Context} createContext `＠private` Initialize a new context.
  * @prop {(error: !Error) => ?} onerror `＠private` Default error handler.
+ * @prop {() => !Object} toJSON Return JSON representation.
+ * @prop {() => !Object} inspect `util.inspect()` implementation, which just returns the JSON output.
  * @typedef {_goa.Middleware} Middleware The function to handle requests which can be installed with the `.use` method.
- * @typedef {(ctx: !_goa.Context, next: !Function) => (!Promise|void)} _goa.Middleware The function to handle requests which can be installed with the `.use` method.
+ * @typedef {(ctx: !_goa.Context, next?: !Function) => (!Promise|void)} _goa.Middleware The function to handle requests which can be installed with the `.use` method.
  */
